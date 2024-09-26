89484ed088b38a9d10dc952277efc69c83a53043
MAHDI-Imen <imenmahdi.studies@gmail.com>
2024-09-26 13:49:18 +0200

    Add requirements and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="octopy",
+    version="1.0",
+    description="Pytorch implementation of octo model",
+    author="Imen Mahdi",
+    author_email="imen.mahdi@inria.fr",
+    packages=find_packages(),
+)
